68fcc69401d7cf4406fc737421803815dc5b8268
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2026-04-24 10:00:23 -0300

    feat: led amarelo que alterna

--- led.ino
+++ led.ino
@@ -1,14 +1,18 @@
 void setup() {
  pinMode(LED_BUILTIN, OUTPUT);
  pinMode (10, OUTPUT);
+ pinMode (4, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(10, LOW);
-digitalWrite(LED_BUILTIN, HIGH);
-delay(1000);
-digitalWrite(LED_BUILTIN, LOW);
-digitalWrite(10, HIGH);
+  digitalWrite(4, LOW);
+digitalWrite(LED_BUILTIN, HIGH); /*liga vermelha e desliga amarela*/
+delay(2000);
+digitalWrite(LED_BUILTIN, LOW); 
+digitalWrite(10, HIGH);/*liga verde e desliga vermelha*/
+delay(2000);
+digitalWrite(4, HIGH);/*liga amarela e desliga verde*/
+digitalWrite(10, LOW);
 delay(1000);
 
 }
