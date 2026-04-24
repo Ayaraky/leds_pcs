402f342cf8d195d2492b57e138d69409a8b68d57
Ayakay <120978042+Ayaraky@users.noreply.github.com>
2026-04-24 09:54:32 -0300

    feat: novo led alternado

--- led.ino
+++ led.ino
@@ -1,11 +1,14 @@
 void setup() {
- pinMode (LED_BUILTIN, OUTPUT);
+ pinMode(LED_BUILTIN, OUTPUT);
+ pinMode (10, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(10, LOW);
 digitalWrite(LED_BUILTIN, HIGH);
 delay(1000);
 digitalWrite(LED_BUILTIN, LOW);
+digitalWrite(10, HIGH);
 delay(1000);
 
 }
